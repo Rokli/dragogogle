124b32b27eb88536999010d3307e32d2a242cca2
satal <satal@DESKTOP-MS43KJ9>
2024-03-08 00:09:01 +0700

    добавил движения поля

--- dragogogle/dragogogle.cpp
+++ dragogogle/dragogogle.cpp
@@ -7,16 +7,25 @@
 #include <random>
 using namespace std;
 string matrix[][15]{
-    {"------------------------"},//0
+    {"----------------"},//0
     {},//1
     {},//2
-    {"$"},//3
-    {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#",},//4
+    {"$"," "," "," "," "," "," "," "," "," "," "," "," "," "},//3
+    {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"}//4
 };
-void PringMatrix() {
-    for (int i = 1; i > 15; ) {
-        matrix[4][i + 1] = "#";
+void PathNext() {
+    if ((rand() % 100 + 0) < 20) {
+        if(matrix[3][14-1] != "|" && matrix[3][14-2] != "|")
+                matrix[3][14] = "|";
     }
+    for (int i = 1; i < 14; i++) {
+        matrix[3][i] = matrix[3][i + 1];
+        matrix[3][i + 1] = " ";
+        matrix[4][i] = matrix[4][i + 1];
+    }
+}
+void PringMatrix() {
+    PathNext();
     for (int i = 0; i < 5; i++) {
         cout << endl;
         for(int j = 0; j < 15;j++)
@@ -25,26 +34,37 @@ void PringMatrix() {
 }
 int main()
 {
+    bool flag = false;
+    srand(time(0));
     PringMatrix();
     while (1) {
         if (GetAsyncKeyState('W') & 0x8000) {
-            matrix[3][0] = "";
+            flag = true;
+            matrix[3][0] = " ";
             string tmp = "$";
             for (int i = 3; i > 1; i--) {
-                matrix[i][0] = "";
+                matrix[i][0] = " ";
                 matrix[i-1][0] = "$";
                 Sleep(1000);
                 system("cls");
                 PringMatrix();
             }
             for (int i = 1; i < 3; i++) {
-                matrix[i][0] = "";
+                matrix[i][0] = " ";
                 matrix[i + 1][0] = "$";
                 Sleep(1000);
                 system("cls");
                 PringMatrix();
             }
         }
+        if (!flag) 
+        {
+            Sleep(1000);
+            system("cls");
+            PathNext();
+            PringMatrix();
+        }
+        flag = false;
         //Sleep(1000);
         //system("cls");
     }
