84cca5fa33d273443ecdbf11eeee3f20e9d674e0
satal <satal@DESKTOP-MS43KJ9>
2024-03-07 22:43:04 +0700

    Добавил прыжок

--- dragogogle/dragogogle.cpp
+++ dragogogle/dragogogle.cpp
@@ -1,7 +1,10 @@
 ﻿// dragogogle.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
+#include <conio.h>
 #include <windows.h>
 #include <iostream>
+#include <string>
+#include <random>
 using namespace std;
 string matrix[][15]{
     {"------------------------"},//0
@@ -11,6 +14,9 @@ string matrix[][15]{
     {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#",},//4
 };
 void PringMatrix() {
+    for (int i = 1; i > 15; ) {
+        matrix[4][i + 1] = "#";
+    }
     for (int i = 0; i < 5; i++) {
         cout << endl;
         for(int j = 0; j < 15;j++)
@@ -19,21 +25,27 @@ void PringMatrix() {
 }
 int main()
 {
+    PringMatrix();
     while (1) {
-        if (GetKeyState('W')) {
+        if (GetAsyncKeyState('W') & 0x8000) {
             matrix[3][0] = "";
             string tmp = "$";
             for (int i = 3; i > 1; i--) {
                 matrix[i][0] = "";
-                matrix[i-1][0] = "";
+                matrix[i-1][0] = "$";
+                Sleep(1000);
+                system("cls");
+                PringMatrix();
             }
             for (int i = 1; i < 3; i++) {
-
+                matrix[i][0] = "";
+                matrix[i + 1][0] = "$";
+                Sleep(1000);
+                system("cls");
+                PringMatrix();
             }
-
         }
-        PringMatrix();
-        Sleep(1000);
-        system("cls");
+        //Sleep(1000);
+        //system("cls");
     }
 }
